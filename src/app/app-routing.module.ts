34b9e87cf3a76411f8275583bc4a18c25ccbfdfb
engineeralihassan <engineralihassan@gmail.com>
2024-01-05 15:48:13 +0500

    The auth pages screens design  done

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'signin',
+    loadChildren: () => import('./signin/signin.module').then( m => m.SigninPageModule)
+  },
+  {
+    path: 'verification',
+    loadChildren: () => import('./verification/verification.module').then( m => m.VerificationPageModule)
+  },
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/verification/verification.page.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verification',
+  templateUrl: './verification.page.html',
+  styleUrls: ['./verification.page.scss'],
+})
+export class VerificationPage implements OnInit {
+
+  otp: any[] = [null, null, null, null,null,null];
+  inputCount = 0;
+  finalInput = '';
+  showSubmitButton = false;
+
+  ngOnInit(): void {
+    this.startInput();
+  }
+
+  startInput(): void {
+    this.inputCount = 0;
+    this.finalInput = '';
+    this.otp = [null, null, null, null];
+    this.enableInput(0);
+  }
+
+  isInputDisabled(index: number): boolean {
+    console.log("this is the condition",index <= this.inputCount)
+    return index < this.inputCount;
+  }
+
+  handleInput(index: number, event: any): void {
+    event.target.value = event.target.value.replace(/[^0-9]/g, '');
+    let { value } = event.target;
+
+    if (value.length == 1) {
+      this.finalInput += value;
+      this.disableInput(index);
+      if (this.inputCount < 5) {
+        this.enableInput(index + 1);
+      }
+      this.inputCount += 1;
+    } else if (value.length == 0 && event.key == 'Backspace') {
+      this.finalInput = this.finalInput.substring(0, this.finalInput.length - 1);
+      this.inputCount = Math.max(0, this.inputCount - 1);
+      this.disableInput(index);
+      if (index > 0) {
+        this.enableInput(index - 1);
+      }
+    } else if (value.length > 1) {
+      event.target.value = value.split('')[0];
+    }
+
+    this.showSubmitButton = true;
+  }
+
+  disableInput(index: number): void {
+    this.otp[index] = +this.finalInput.charAt(index); // Convert to number
+    document.getElementById(`otp-input-${index}`)?.blur();
+  }
+
+  enableInput(index: number): void {
+    if (index >= 0 && index < 6) {
+      document.getElementById(`otp-input-${index}`)?.focus();
+    }
+  }
+
+  validateOTP(): void {
+    alert('Success');
+  }
+
+  onKeyboardButtonClick(digit: number): void {
+    if (this.inputCount < 6) {
+      this.otp[this.inputCount] = digit;
+      this.inputCount++;
+      this.showSubmitButton = false;
+      this.enableInput(this.inputCount);
+    }
+  }
+  
+
+}
