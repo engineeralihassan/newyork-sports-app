ab16b6ce28096213fd901603a46683865f0079ab
engineeralihassan <engineralihassan@gmail.com>
2024-01-09 15:21:22 +0500

    The all pages are done ui is done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -22,7 +22,7 @@ export class HomePage {
     }
   }
   isLoggedin=false;
-  matches=[1,2,3,4,5,6,7,8]
+  matches=[1,2,3,4,5,6,7,8,9,10,11,1,2,13,14,15,16,22,33,44,55,66,77,88,90]
 
   constructor(private router:Router, private authService:AuthService) {
     console.log("The loggen is uer::",this.authService.isLoggedin);
