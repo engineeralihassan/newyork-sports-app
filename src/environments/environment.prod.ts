16070195f8bbc52b1741a9a72463ad5031f15ad0
engineeralihassan <engineralihassan@gmail.com>
2024-01-30 12:31:55 +0500

    button font issue fixed in reset password page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://139.59.35.241:3270',
-  admin: 'http://139.59.35.241:3280',
+  apiUrl: 'http://192.168.1.180:3270',
+  admin: 'http://192.168.1.180:3280',
 };
