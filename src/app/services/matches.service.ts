918a638968319d12dab0fc9f36c41dc702aecc80
engineeralihassan <engineralihassan@gmail.com>
2024-02-01 12:18:01 +0500

    Location from capacitor setup set

--- src/app/services/matches.service.ts
+++ src/app/services/matches.service.ts
@@ -1,31 +1,37 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Device } from '@capacitor/device';
 import { Router } from '@angular/router';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Geolocation } from '@capacitor/geolocation';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MatchesService {
-  public photos: any[]= [];
-  photo:any;
+  public photos: any[] = [];
+  photo: any;
   private myObject: any;
   private objectSubject = new BehaviorSubject<any>(null);
   private photoSubject = new BehaviorSubject<any>(null);
-  constructor(private router:Router,private http:HttpClient){}
-  checkinFail=false;
-  apiUrl=environment.apiUrl;
+  constructor(private router: Router, private http: HttpClient) {}
+  checkinFail = false;
+  apiUrl = environment.apiUrl;
   setObject(obj: any): void {
     this.myObject = obj;
-    this.objectSubject.next(obj); 
+    this.objectSubject.next(obj);
   }
   setPhoto(obj: any): void {
     this.photo = obj;
-    this.photoSubject.next(obj); 
+    this.photoSubject.next(obj);
   }
 
   getObject(): any {
@@ -39,34 +45,60 @@ export class MatchesService {
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
-      quality: 100
+      quality: 100,
     });
-    const blob = await fetch(capturedPhoto.webPath!).then(res => res.blob());
-    const file = new File([blob], "captured_image.png", { type: "image/png" });
-    this.photos.unshift(file); 
+    const blob = await fetch(capturedPhoto.webPath!).then((res) => res.blob());
+    const file = new File([blob], 'captured_image.png', { type: 'image/png' });
+    this.photos.unshift(file);
     this.router.navigate(['selfie/screen2']);
   }
-   takePicture = async () => {
+  takePicture = async () => {
     const image = await Camera.getPhoto({
       quality: 90,
       allowEditing: true,
-      resultType: CameraResultType.Uri
+      resultType: CameraResultType.Uri,
     });
     var imageUrl = image.webPath;
-
   };
-  getPhoto(data:any){
+  printCurrentPosition = async () => {
+    const coordinates = await Geolocation.getCurrentPosition();
+
+    console.log('Current position:', coordinates);
+  };
+
+  getPhoto(data: any) {
     return this.photo;
   }
-    ProceedMatch(data:any): Observable<any> {
-      let token=localStorage.getItem('nasaTocken');
-      let  headers = new HttpHeaders({
-        'Authorization': `Bearer ${token}` 
-      });
-      return this.http.post(`${this.apiUrl}/verifyCoordinates`, data, {headers} );
+  ProceedMatch(data: any): Observable<any> {
+    let token = localStorage.getItem('nasaTocken');
+    let headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    return this.http.post(`${this.apiUrl}/verifyCoordinates`, data, {
+      headers,
+    });
+  }
+
+  async checkAndRequestLocationPermission() {
+    const permissionStatus = await Geolocation.checkPermissions();
+
+    if (permissionStatus.location === 'granted') {
+      // Location permission already granted, proceed with location functionality
+      console.log('Location permission granted');
+    } else {
+      // Location permission not granted, request it
+      const requestResult = await Geolocation.requestPermissions();
+
+      if (requestResult.location === 'granted') {
+        console.log('Location permission granted');
+        // Proceed with location functionality
+      } else {
+        console.log('Location permission denied');
+        // Handle denial of location permission gracefully
+      }
+    }
   }
 }
-
 export interface UserPhoto {
   filepath: string;
   webviewPath?: string;
