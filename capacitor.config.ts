eb1eff977f5c6df3b761e663b1e1c0b5a4c2a852
engineeralihassan <engineralihassan@gmail.com>
2024-02-05 12:54:27 +0500

    The chnages in capacior plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'new-york-app-01',
-  appName: 'newyork-sports-app',
+  appName: 'NYSA',
   webDir: 'www',
   server: {
     androidScheme: 'http',
