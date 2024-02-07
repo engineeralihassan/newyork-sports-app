1a88997eef661c98f03aa708769744e52bbe43b4
engineeralihassan <engineralihassan@gmail.com>
2024-02-07 12:14:58 +0500

    user

--- src/app/admin/single-user/single-user.component.ts
+++ src/app/admin/single-user/single-user.component.ts
@@ -2,6 +2,7 @@ import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from 'src/app/services/admin.service';
 import { environment } from 'src/environments/environment';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-single-user',
   templateUrl: './single-user.component.html',
@@ -12,7 +13,12 @@ export class SingleUserComponent implements OnInit {
   userData: any;
   loading: any = false;
   url = environment.admin;
-  constructor(private adminService: AdminService, private datePipe: DatePipe) {
+
+  constructor(
+    private adminService: AdminService,
+    private datePipe: DatePipe,
+    private location: Location
+  ) {
     this.user = adminService.singleUser;
   }
 
@@ -23,6 +29,9 @@ export class SingleUserComponent implements OnInit {
   transformDate(dateString: string): string | null {
     const date = new Date(dateString);
     return this.datePipe.transform(date, 'EEE, MMMM dd, yyyy');
+  }
+  goBack() {
+    this.location.back();
   }
   singleUser(user: any) {
     this.loading = true;
